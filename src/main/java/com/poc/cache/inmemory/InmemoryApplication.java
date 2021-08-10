e0f002a51100b0440d673e447ef633fd7e5b3ec1
Artur Drummond <arturbdr@gmail.com>
2021-08-10 19:45:42 +0200

    enabling cache to be managed by spring

--- src/main/java/com/poc/cache/inmemory/InmemoryApplication.java
+++ src/main/java/com/poc/cache/inmemory/InmemoryApplication.java
@@ -2,8 +2,10 @@ package com.poc.cache.inmemory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class InmemoryApplication {
 
     public static void main(String[] args) {
